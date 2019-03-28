bd005e9f5a333da02c2ff3361630f05ef10f313a
Andreii <andreyhodirev@gmail.com>
2019-03-28 10:30:23 +0200

    create seed admin (run - php artisan make:seed )

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@dkpstat.com',
+            'password' => bcrypt(ENV('ADMIN_PASSWORD')),
+            'is_confirmed' => 1,
+            'is_admine_CH777' => 1,
+            'phone' =>'000000000',
+
+        ]);
         // $this->call(UsersTableSeeder::class);
     }
 }
